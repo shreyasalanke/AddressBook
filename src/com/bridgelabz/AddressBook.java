8ff93061d6f5e43f0214e17f9f7512c908dd0fc7
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-29 11:23:24 +0530

    ADDED: welcome to Address Book

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.util.*;
+
+public class AddressBook 
+   {
+     public static void main(String[] args) 
+     {
+		System.out.println("welcome to Address Book");
+	 }
+
+   }
