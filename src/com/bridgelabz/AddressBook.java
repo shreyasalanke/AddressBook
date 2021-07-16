5c189f7a6bccbf0ba8011b8ceed7e280dd9e77f8
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-16 23:40:14 +0530

    ADDED: program to add person details

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,64 +1,115 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
-import java.io.Console;
-import java.util.*;
 
-public class AddressBook 
-   {
-	private String firstname;
-	private String lastname;
-	private String address;
-	private String city;
-	private String state;
-	private String zip;
-	private String phonenumber;
-	private String emailid;
-	Scanner sc =  new Scanner(System.in);
- 
-	AddressBook(String firstname,String lastname,String address,String city,String state,String zip,String phonenumber,String emailid)
+public class AddressBook {
+	private  String FirstName;
+	   private String LastName;
+	   private String State;
+	   private String City;
+	   private String Address;
+	   private long ZipCode;
+	   private long PhoneNO;
+	   private String Email;
+
+
+	  Scanner userinput = new Scanner(System.in);
+	   public String getFirstName()
+	   {
+	     return FirstName;
+	   }
+	   public void setFirstName(String firstName)
+	   {
+	     FirstName = firstName;
+	   }
+	  public String getLastName()
+	  {
+	     return LastName;
+	  }
+	  public void setLastName(String lastName)
+	  {
+	     LastName = lastName;
+	   }
+	  public String getState() 
+	  {
+	     return State;
+	   }
+	  public void setState(String state)
+	   {
+	     State = state;
+	    }
+	  public String getCity()
+	   {
+	     return City;
+	    }
+	  public void setCity(String city)
+	   {
+	       City = city;
+	    }
+	  public String getAddress()
+	    {
+	       return Address;
+	     }
+	  public void setAddress(String address)
+	   {
+		Address = address;
+	    }
+	  public long getZipCode()
+	   {
+	      return ZipCode;
+	    }
+	  public void setZipCode(long zipCode)
+	    { 
+	        ZipCode = zipCode;
+		}
+	   public long getPhoneNO()
+	    {
+	        return PhoneNO;
+		}
+	   public void setPhoneNO(long phoneNO)
+	   {
+	       PhoneNO = phoneNO;
+	      }
+	   public String getEmail()
+	    {
+		return Email;
+	     }
+	  public void setEmail(String email)
+	   {  
+	       Email = email;
+	      }
+	public void contactDisplay()
 	{
-		this.firstname = "";
-		this.lastname = "";
-		this.address = "";
-		this.city = "";
-		this.state = "";
-		this.zip = "";
-		this.phonenumber = "";
-		this.emailid = "";
-    }
-	
-	AddressBook()
+	    System.out.println("Enter a first name: ");  
+	    setFirstName(userinput.nextLine()); 
+	    System.out.println("Enter a last name: ");
+	    setLastName(userinput.nextLine());
+	    System.out.println("Enter a state: ");
+	    setState(userinput.next());
+	    System.out.println("Enter a city: ");
+	    setCity(userinput.next());
+	    System.out.println("Enter a address: ");
+	    setAddress(userinput.next());
+	    System.out.println("Enter a zipcode: ");
+	    setZipCode(userinput.nextLong());
+	    System.out.println("Enter a phoneno: ");
+	    setPhoneNO(userinput.nextLong());
+	    System.out.println("Enter a email: ");
+	    setEmail(userinput.next());
+	    System.out.println("name: " + this.getFirstName());
+	    System.out.println("last name: " + this.getLastName()); 
+	    System.out.println("state: " + this.getState());
+	    System.out.println("zipcode: " + this.getZipCode());
+	    System.out.println("city: " + this.getCity()); 
+	    System.out.println("phone_no: " + this.getPhoneNO());
+	    System.out.println("email: " + this.getEmail());
+	    System.out.println("email: " + this.getEmail());
+
+	}
+
+	public static void main(String[] args)
 	{
-		firstname = "";
-        lastname = "";
-        address = "";
-        city = "";
-        state = "";
-        zip = "";
-        phonenumber = "";
-        emailid = "";
-    }
-	
-	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first name: ");
-        String firstName = scanner.next();
-        System.out.print("Enter last name: ");
-        String lastName = scanner.next();
-        System.out.print("Enter address: ");
-        String address = scanner.next();
-        System.out.print("Enter city: ");
-        String city = scanner.next();
-        System.out.print("Enter state: ");
-        String state = scanner.next();
-        System.out.print("Enter zip: ");
-        long zip = scanner.nextLong();
-        System.out.print("Enter phone no: ");
-        long phonenumber = scanner.nextLong();
-        System.out.print("Enter email: ");
-        String emailid = scanner.next();
-        
-        
-      }
-   }
+    System.out.println("Welcome to the Address Book System Program");
+	   AddressBook person = new  AddressBook();
+	   person.contactDisplay();
+	}
+}
